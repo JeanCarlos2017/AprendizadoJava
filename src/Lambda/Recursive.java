4396b93382a37328fc3d4d243503f050a85ba34b
JeanCarlos2017 <jean__100limites@hotmail.com>
2020-08-18 20:35:12 -0300

    Update Recursive.java
    acrescentando uma função lambda recursiva

--- src/Lambda/Recursive.java
+++ src/Lambda/Recursive.java
@@ -1,12 +1,17 @@
 package Lambda;
 
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.IntToDoubleFunction;
 
 // Recursive.java
 // @param <I> - Functional Interface Type
 public class Recursive<I> {
    public I func;
+   private static Map<Integer, Long> MAPA_FATORIAL = new HashMap<>();
 
     // Test.java
     static double factorial(int n) {
@@ -17,10 +22,39 @@ public class Recursive<I> {
         return recursive.func.applyAsDouble(n);
     }
 
+    Function<Integer, Long> fact = x -> {
+        BiFunction<BiFunction, Integer, Long> factHelper =
+                (f, d) -> (d == 0) ? 1 :
+                        d*(long)f.apply(f,d-1);
+
+        return factHelper.apply(factHelper, x);
+    };
+
+    static Function<Integer, Long> fatorialRecursiveMemoizing= x -> {
+        BiFunction<BiFunction, Integer, Long> factHelper =
+                (f, d) -> {
+                    if (d == 0) return 1L;
+                    if (MAPA_FATORIAL.containsKey(d)) return MAPA_FATORIAL.get(d);
+                    Long resultado = d*(long)f.apply(f,d-1);
+                    MAPA_FATORIAL.put(d, resultado);
+                    return resultado;
+                };
+        return factHelper.apply(factHelper, x);
+    };
 
 
     public static void main (String args[]){
-        System.out.println(factorial(6));
+        //System.out.println(Recursive.fatorialRecursiveMemoizing.apply(3));
+        //testando a eficcácia do mapeamento
+        long start = System.nanoTime();
+        System.out.println(Recursive.fatorialRecursiveMemoizing.apply(15));
+        long end = System.nanoTime();
+        System.out.println("Tempo gasto em nanosegundos sem mapeamento foi: "+(end-start));
+
+        start = System.nanoTime();
+        System.out.println(Recursive.fatorialRecursiveMemoizing.apply(19));
+        end = System.nanoTime();
+        System.out.println("Tempo gasto em nanosegundos com mapeamento foi: "+(end-start));
     }
 
 }
